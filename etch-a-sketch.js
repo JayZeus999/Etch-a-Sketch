844d0e94de438051a6a8f15cdd9ea57ec20c5deb
Falade Joseph <faladejoseph400@gmail.com>
2025-09-29 18:49:36 +0100

    Set squares dimensions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,24 +5,31 @@ pxbtn.textContent = "Set Grid";
 document.body.appendChild(pxbtn);
 
 pxbtn.addEventListener("click", () => {
-    let gridask = prompt("What grid specs do you want?");
-    let n = parseInt(gridask);
+    let gridAsk = prompt("What grid specs do you want?");
+    let n = parseInt(gridAsk);
+
+    container.innerHTML = "";
+
+    let squareSize = 600 / n;
 
     for (j = 0; j < n; j++) {
-    for (i = 0; i < n; i++) {
-        const sq = document.createElement("div");
-        sq.classList.add("square");
+        for (i = 0; i < n; i++) {
+            const sq = document.createElement("div");
+            sq.classList.add("square");
 
-        sq.addEventListener("mouseenter", () => {
-            sq.style.backgroundColor = "pink";
-        });
+            sq.addEventListener("mouseenter", () => {
+                sq.style.backgroundColor = "pink";
+            });
 
-        sq.addEventListener("mouseleave", () => {
-            sq.style.backgroundColor = "blue";
-        });
+            sq.addEventListener("mouseleave", () => {
+                sq.style.backgroundColor = "blue";
+            });
 
-        container.appendChild(sq);
+            sq.style.height = `${squareSize}px`
+            sq.style.width = `${squareSize}px`
+            
+            container.appendChild(sq);
+        }
     }
-}
 
 });
