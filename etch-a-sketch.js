df481d39e0ff35628a2a832a115c3936a868f5e2
Falade Joseph <faladejoseph400@gmail.com>
2025-09-29 18:14:13 +0100

    Create button for size prompt

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,13 @@
 const container = document.querySelector("#container");
 
+const pxbtn = document.createElement("button");
+pxbtn.textContent = "Set Grid";
+document.body.appendChild(pxbtn)
+
+pxbtn.addEventListener("click", () => {
+    let gridask = prompt("What grid specs do you want?");
+});
+
 for (j = 0; j < 16; j++) {
     for (i = 0; i < 16; i++) {
         const sq = document.createElement("div");
@@ -12,7 +20,7 @@ for (j = 0; j < 16; j++) {
         sq.addEventListener("mouseleave", () => {
             sq.style.backgroundColor = "blue";
         });
-        
+
         container.appendChild(sq);
     }
 }
