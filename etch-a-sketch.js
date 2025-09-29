55deca83d401e9f1dd395f4531a52deb250e3664
Falade Joseph <faladejoseph400@gmail.com>
2025-09-29 16:54:12 +0100

    Add square loop & margin

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,11 @@
-const sqr1 = document.querySelector("#container");
-const sq11 = document.createElement("div");
-sq11.classList.add("square");
-container.appendChild(sq11);
+const container = document.querySelector("#container");
+
+for (i = 0; i <= 16; i++){
+    const sq11 = document.createElement("div");
+    sq11.classList.add("square");
+    container.appendChild(sq11);
+}
+
+// const sq12 = document.createElement("div");
+// sq12.classList.add("square");
+// container.appendChild(sq12);
