a6f3579792fc01c05893e095854543a7dbc03784
Falade Joseph <faladejoseph400@gmail.com>
2025-09-29 18:15:40 +0100

    Set loop counters to prompts

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,14 +2,14 @@ const container = document.querySelector("#container");
 
 const pxbtn = document.createElement("button");
 pxbtn.textContent = "Set Grid";
-document.body.appendChild(pxbtn)
+document.body.appendChild(pxbtn);
 
 pxbtn.addEventListener("click", () => {
     let gridask = prompt("What grid specs do you want?");
-});
+    let n = parseInt(gridask);
 
-for (j = 0; j < 16; j++) {
-    for (i = 0; i < 16; i++) {
+    for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) {
         const sq = document.createElement("div");
         sq.classList.add("square");
 
@@ -24,3 +24,5 @@ for (j = 0; j < 16; j++) {
         container.appendChild(sq);
     }
 }
+
+});
